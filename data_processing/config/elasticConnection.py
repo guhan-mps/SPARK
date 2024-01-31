59e52f99c70007ba5d59b14a00933084a4216a83
guhan-mps <guhan@madstreetden.com>
2024-01-31 14:44:34 +0530

    Elastic deployment done

--- data_processing/config/elasticConnection.py
+++ data_processing/config/elasticConnection.py
@@ -8,8 +8,12 @@ def elastic_connect():
     Create a python client to the Elastic search deployment using the deployment's cloud_id, the username and password
     for the elastic search service account
     """
+    try:
+        cloud_id=os.getenv('CLOUD_ID')
+    except:
+        cloud_id=os.getenv('CLOUD_ID_1')+os.getenv('CLOUD_ID_2')+os.getenv('CLOUD_ID_3')
     client = Elasticsearch(
-        cloud_id=os.getenv('CLOUD_ID'),
+        cloud_id=cloud_id,
         basic_auth=(os.getenv('ELASTIC_USER'), os.getenv('ELASTIC_PASSWORD'))
     )
     return client
